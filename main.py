5f988394d64271209fb24ec3b364a25fc94cfe6b
D3f4ult <d3f4ult@D3f4ults-MacBook-Air.local>
2025-04-01 20:41:41 +0200

    updates main to listen to all interfaces

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from app import create_app
 
 def main():
-    """
-    This is the entry point to run the Study Together application.
-    For a better development experience, use 'flask run' instead.
-    """
-    app = create_app()
-    app.run(debug=True)
 
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == "__main__":
     main()
